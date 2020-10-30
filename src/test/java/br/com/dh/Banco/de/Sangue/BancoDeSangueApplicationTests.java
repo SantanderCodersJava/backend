c89b3274ee11be37b2c729cc03c0d2e1c3057795
David Chaves <davi.ch.fe@gmail.com>
2020-10-30 15:29:50 -0300

    Começando o projeto backend

---
+++ src/test/java/br/com/dh/Banco/de/Sangue/BancoDeSangueApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dh.Banco.de.Sangue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoDeSangueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
