8a749264e952d16624682412a1d1ce16b5baab91
Erika Lima <erikalmlima@gmail.com>
2020-10-31 13:26:57 -0300

    Entidade, repositorio e controller Banco de sangue

---
+++ src/main/java/br/com/dh/Banco/de/Sangue/repository/BancoDeSangueRepository.java
@@ -0,0 +1,9 @@
+package br.com.dh.Banco.de.Sangue.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.dh.Banco.de.Sangue.model.BancoDeSangue;
+
+public interface BancoDeSangueRepository extends JpaRepository <BancoDeSangue, Integer>{
+
+}
