92f4c1c498b37fc4fb6dcf59e1dd5dbe26f63dc0
Thiago Silva <danielareis.ufabc@gmail.com>
2020-11-11 18:45:05 -0300

    adicionadas as classes de service referentes ao banco de sangue

---
+++ src/main/java/br/com/dh/Banco/de/Sangue/service/BancoServiceImpl.java
@@ -0,0 +1,58 @@
+package br.com.dh.Banco.de.Sangue.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import br.com.dh.Banco.de.Sangue.exception.SenhaInvalidaException;
+import br.com.dh.Banco.de.Sangue.model.BancoDeSangue;
+import br.com.dh.Banco.de.Sangue.repository.BancoDeSangueRepository;
+
+@Service
+public class BancoServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private BancoDeSangueRepository bancoDeSangueRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public BancoDeSangue cadastrar(BancoDeSangue bancoSangue) {
+        String senhaCriptografada = passwordEncoder.encode(bancoSangue.getSenha());
+        bancoSangue.setSenha(senhaCriptografada);
+        return bancoDeSangueRepository.save(bancoSangue);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        // pega o banco e verifica se o email dele existe
+        BancoDeSangue bancoSangue = bancoDeSangueRepository.findByEmail(email)
+                .orElseThrow( ()-> new UsernameNotFoundException("Cadastro não encontrado na base de dados"));
+
+        // da a permissão ao banco de usuario comum
+        String[] permissaoBanco = new String[]{"USER"};
+
+        // retorna um UserDetail com os dados do banco
+        return User
+                .builder()
+                .username(bancoSangue.getEmail())
+                .password(bancoSangue.getSenha())
+                .roles(permissaoBanco)
+                .build();
+    }
+
+    public UserDetails autenticar(BancoDeSangue bancoSangue){
+        UserDetails user = loadUserByUsername(bancoSangue.getEmail());
+        boolean senhasBatem = passwordEncoder.matches( bancoSangue.getSenha(), user.getPassword());
+
+        if (senhasBatem){
+            return user;
+        }
+
+        throw new SenhaInvalidaException();
+    }
+}
