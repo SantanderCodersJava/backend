360b507a35a098795e33678a1b86260d928f78b2
rodriguespaula1003 <62486100+rodriguespaula1003@users.noreply.github.com>
2020-11-17 17:11:18 -0300

    implementando upload de arquivos no cadastro de doador

--- src/main/java/br/com/dh/Banco/de/Sangue/controller/BancoDeSangueController.java
+++ src/main/java/br/com/dh/Banco/de/Sangue/controller/BancoDeSangueController.java
@@ -1,10 +1,13 @@
 package br.com.dh.Banco.de.Sangue.controller;
 
+import java.io.IOException;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,8 +16,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 
 import br.com.dh.Banco.de.Sangue.dto.CredenciaisDTO;
@@ -24,6 +29,7 @@ import br.com.dh.Banco.de.Sangue.model.BancoDeSangue;
 import br.com.dh.Banco.de.Sangue.repository.BancoDeSangueRepository;
 import br.com.dh.Banco.de.Sangue.service.BancoDeSangueServiceImpl;
 import br.com.dh.Banco.de.Sangue.service.JwtServiceBanco;
+import br.com.dh.Banco.de.Sangue.utils.FileUploadUtil;
 
 @CrossOrigin
 @RestController
@@ -63,6 +69,29 @@ public class BancoDeSangueController {
         } catch (UsernameNotFoundException | SenhaInvalidaException e){
             throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, e.getMessage());
         }
+    }
+	
+
+    @CrossOrigin
+    @PostMapping("/upload")
+    public String saveFile(@RequestParam("image") MultipartFile file) {
+    	String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+    	String uploadDir = "files";
+    	Date date = new Date();
+    	String filePrefix = date.getTime() + "-";
+    	
+    	fileName = filePrefix + fileName;
+    	
+    	try {
+    		FileUploadUtil.saveFile(uploadDir, fileName, file);
+    	}
+    	catch(IOException e) {
+    		System.out.println("O arquivo não foi salvo" + e);
+    		return "Error: " + e; 
+    	}
+    	
+    	System.out.println("O arquivo foi salvo");
+    	return uploadDir + "/" + fileName;
     }
 	
 	 @DeleteMapping(value = "/{id}")
